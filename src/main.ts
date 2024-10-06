9437f8652d276ade480ff2f8135649b746676996
ljcui1 <yangyangjiaocui@gmail.com>
2024-10-06 14:32:27 -0700

    step 10 complete (descriptions are shown when hovering over buttons)

--- src/main.ts
+++ src/main.ts
@@ -11,25 +11,30 @@ interface Item {
   cost: number;
   rate: number;
   amount: number;
+  desc: string;
 }
 
 const gameName = "Rat Gains Clicker";
 document.title = gameName;
 
 const availableItems: Item[] = [
-  { label: "Buy Rat Wheel", cost: 10, rate: 0.1, amount: 0 },
-  { label: "Buy Lab Blocks", cost: 100, rate: 2, amount: 0 },
-  { label: "Buy Rat-Sized Barbells", cost: 1000, rate: 50, amount: 0 },
+  { label: "Buy Rat Wheel", cost: 10, rate: 0.1, amount: 0, desc: "a rat treadmill used to train cardiovascular health" },
+  { label: "Buy Lab Blocks", cost: 100, rate: 2, amount: 0, desc: "lab-created blocks of food and all necessary nutrition for an adult rat" },
+  { label: "Buy Rat-Sized Barbells", cost: 1000, rate: 50, amount: 0, desc: "barbell weights for muscle gainz" },
+  { label: "Buy Rat Creatine", cost: 10000, rate: 100, amount: 0, desc: "supplement for optimal muscle growth" },
+  { label: "Buy Rat Steroids", cost: 1000000, rate: 5000, amount: 0, desc: "bro you're too far gone..." },
 ];
 
 const ratButtName = "🐀";
 const wheel = availableItems[0];
 const block = availableItems[1];
 const bar = availableItems[2];
+const creatine = availableItems[3];
+const steroids = availableItems[4];
 
 let counter: number = 0;
 let lastFrame: number = 0;
-let growthRate: number = 1;
+let growthRate: number = 0;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
@@ -44,12 +49,13 @@ ratButton.innerHTML = ratButtName;
 app.append(ratButton);
 
 const countTxt = document.createElement("h2");
-countTxt.innerHTML = "Rat Counter: " + counter;
+countTxt.innerHTML = "Rat Gainz Counter: " + counter;
 countInfo.append(countTxt);
 app.append(countInfo);
 
 const wheelButton: HTMLButtonElement = document.createElement("button");
 wheelButton.innerHTML = wheel.label + " $" + wheel.cost.toFixed(2);
+wheelButton.title = wheel.desc;
 upgrades.append(wheelButton);
 const wheelCount = document.createElement("h3");
 wheelCount.innerHTML = "# of Wheels: " + wheel.amount;
@@ -57,6 +63,7 @@ upgradeInfo.append(wheelCount);
 
 const blockButton: HTMLButtonElement = document.createElement("button");
 blockButton.innerHTML = block.label + " $" + block.cost.toFixed(2);
+blockButton.title = block.desc;
 upgrades.append(blockButton);
 const blockCount = document.createElement("h3");
 blockCount.innerHTML = "# of Blocks: " + block.amount;
@@ -64,16 +71,37 @@ upgradeInfo.append(blockCount);
 
 const barButton: HTMLButtonElement = document.createElement("button");
 barButton.innerHTML = bar.label + " $" + bar.cost.toFixed(2);
+barButton.title = bar.desc;
 upgrades.append(barButton);
 app.append(upgrades);
 const barCount = document.createElement("h3");
 barCount.innerHTML = "# of Barbells: " + bar.amount;
 upgradeInfo.append(barCount);
+
+const crButton: HTMLButtonElement = document.createElement("button");
+crButton.innerHTML = creatine.label + " $" + creatine.cost.toFixed(2);
+crButton.title = creatine.desc;
+upgrades.append(crButton);
+app.append(upgrades);
+const crCount = document.createElement("h3");
+crCount.innerHTML = "# of Creatine: " + creatine.amount;
+upgradeInfo.append(crCount);
+
+const stButton: HTMLButtonElement = document.createElement("button");
+stButton.innerHTML = steroids.label + " $" + steroids.cost.toFixed(2);
+stButton.title = steroids.desc;
+upgrades.append(stButton);
+app.append(upgrades);
+const stCount = document.createElement("h3");
+stCount.innerHTML = "# of Steroids: " + steroids.amount;
+upgradeInfo.append(stCount);
 app.append(upgradeInfo);
 
 wheelButton.disabled = true;
 blockButton.disabled = true;
 barButton.disabled = true;
+crButton.disabled = true;
+stButton.disabled = true;
 
 ratButton.addEventListener("click", updateCounter);
 wheelButton.addEventListener("click", () => {
@@ -97,7 +125,7 @@ blockButton.addEventListener("click", () => {
   block.cost *= 1.15;
   blockButton.innerHTML = block.label + " $" + block.cost.toFixed(2);
   requestAnimationFrame(animCounter);
-  wheelButton.disabled = true;
+  blockButton.disabled = true;
 });
 
 barButton.addEventListener("click", () => {
@@ -109,7 +137,31 @@ barButton.addEventListener("click", () => {
   bar.cost *= 1.15;
   barButton.innerHTML = bar.label + " $" + bar.cost.toFixed(2);
   requestAnimationFrame(animCounter);
-  wheelButton.disabled = true;
+  barButton.disabled = true;
+});
+
+crButton.addEventListener("click", () => {
+    creatine.amount++;
+    crCount.innerHTML = "# of Creatine: " + creatine.amount;
+    counter -= creatine.cost;
+    growthRate += creatine.rate;
+    txt.innerHTML = "Rat Growth Rate: " + growthRate.toFixed(1) + " gains/sec";
+    creatine.cost *= 1.15;
+    crButton.innerHTML = creatine.label + " $" + creatine.cost.toFixed(2);
+    requestAnimationFrame(animCounter);
+    crButton.disabled = true;
+  });
+
+stButton.addEventListener("click", () => {
+  steroids.amount++;
+  stCount.innerHTML = "# of Steroids: " + steroids.amount;
+  counter -= steroids.cost;
+  growthRate += steroids.rate;
+  txt.innerHTML = "Rat Growth Rate: " + growthRate.toFixed(1) + " gains/sec";
+  steroids.cost *= 1.15;
+  stButton.innerHTML = steroids.label + " $" + steroids.cost.toFixed(2);
+  requestAnimationFrame(animCounter);
+  stButton.disabled = true;
 });
 
 function updateCounter() {
@@ -122,14 +174,32 @@ function updateDisplay() {
 
   if (counter >= wheel.cost) {
     wheelButton.disabled = false;
+  }else{
+    wheelButton.disabled = true;
   }
 
   if (counter >= block.cost) {
     blockButton.disabled = false;
+  }else{
+    blockButton.disabled = true;
   }
 
   if (counter >= bar.cost) {
     barButton.disabled = false;
+  }else{
+    barButton.disabled = true;
+  }
+
+  if (counter >= creatine.cost) {
+    crButton.disabled = false;
+  }else{
+    crButton.disabled = true;
+  }
+
+  if (counter >= steroids.cost) {
+    stButton.disabled = false;
+  }else{
+    stButton.disabled = true;
   }
 }
 
