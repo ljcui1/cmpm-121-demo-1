84695f6007fb00e187d3584a95c92806e77c9f1c
ljcui1 <yangyangjiaocui@gmail.com>
2024-10-06 13:50:04 -0700

    step 7 complete

--- src/main.ts
+++ src/main.ts
@@ -22,6 +22,10 @@ let wheelNum = 0;
 let blockNum = 0;
 let barNum = 0;
 
+let wheelPrice = 10;
+let blockPrice = 100;
+let barPrice = 1000;
+
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
@@ -40,21 +44,21 @@ info.append(countTxt);
 app.append(info);
 
 const wheelButton: HTMLButtonElement = document.createElement("button");
-wheelButton.innerHTML = wheelName;
+wheelButton.innerHTML = wheelName + " $" + wheelPrice.toFixed(2);
 upgrades.append(wheelButton);
 const wheelCount = document.createElement("h3");
 wheelCount.innerHTML = "# of Wheels: " + wheelNum;
 upgradeInfo.append(wheelCount);
 
 const blockButton: HTMLButtonElement = document.createElement("button");
-blockButton.innerHTML = blockName;
+blockButton.innerHTML = blockName + " $" + blockPrice.toFixed(2);
 upgrades.append(blockButton);
 const blockCount = document.createElement("h3");
 blockCount.innerHTML = "# of Blocks: " + blockNum;
 upgradeInfo.append(blockCount);
 
 const barButton: HTMLButtonElement = document.createElement("button");
-barButton.innerHTML = barName;
+barButton.innerHTML = barName + " $" + barPrice.toFixed(2);
 upgrades.append(barButton);
 app.append(upgrades);
 const barCount = document.createElement("h3");
@@ -73,6 +77,8 @@ wheelButton.addEventListener("click", () => {
   counter -= 10;
   growthRate += 0.1;
   txt.innerHTML = "Rat Growth Rate: " + growthRate.toFixed(1) + " gains/sec";
+  wheelPrice *= 1.15;
+  wheelButton.innerHTML = wheelName + " $" + wheelPrice.toFixed(2);
   requestAnimationFrame(animCounter);
   wheelButton.disabled = true;
 });
@@ -83,6 +89,8 @@ blockButton.addEventListener("click", () => {
   counter -= 100;
   growthRate += 2;
   txt.innerHTML = "Rat Growth Rate: " + growthRate.toFixed(1) + " gains/sec";
+  blockPrice *= 1.15;
+  blockButton.innerHTML = blockName + " $" + blockPrice.toFixed(2);
   requestAnimationFrame(animCounter);
   wheelButton.disabled = true;
 });
@@ -93,6 +101,8 @@ barButton.addEventListener("click", () => {
   counter -= 1000;
   growthRate += 50;
   txt.innerHTML = "Rat Growth Rate: " + growthRate.toFixed(1) + " gains/sec";
+  barPrice *= 1.15;
+  barButton.innerHTML = barName + " $" + barPrice.toFixed(2);
   requestAnimationFrame(animCounter);
   wheelButton.disabled = true;
 });
@@ -105,15 +115,15 @@ function updateCounter() {
 function updateDisplay() {
   countTxt.innerHTML = "Rat Counter: " + counter.toFixed(2);
 
-  if (counter >= 10) {
+  if (counter >= wheelPrice) {
     wheelButton.disabled = false;
   }
 
-  if (counter >= 100) {
+  if (counter >= blockPrice) {
     blockButton.disabled = false;
   }
 
-  if (counter >= 1000) {
+  if (counter >= barPrice) {
     barButton.disabled = false;
   }
 }
